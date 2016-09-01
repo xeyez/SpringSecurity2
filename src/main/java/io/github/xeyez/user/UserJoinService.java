3b4422368aab07d72d97a6a86ac67d6169291147
xeyez <xeyez@naver.com>
2016-09-01 17:43:16 +0900

    add exception processing

--- src/main/java/io/github/xeyez/user/UserJoinService.java
+++ src/main/java/io/github/xeyez/user/UserJoinService.java
@@ -6,6 +6,7 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,14 +14,30 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 public class UserJoinService {
 	private UserDetailsManager userDetailsManager;
+	private PasswordEncoder passwordEncoder;
 
 	public void setUserDetailsManager(UserDetailsManager userDetailsManager) {
 		this.userDetailsManager = userDetailsManager;
 	}
+	
+	public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
 
 	@Transactional
 	public void join(NewUser newUser) {
-		UserDetails user = new User(newUser.getName(), newUser.getPassword(),
+		String id = newUser.getName();
+		if(id.contains("admin"))
+			throw new UnavailableIDException("\"admin\"은 포함될 수 없습니다.");
+		if(id.contains("manager"))
+			throw new UnavailableIDException("\"manager\"는 포함될 수 없습니다.");
+		else if (!id.matches("[0-9|a-z|A-Z]*"))
+			throw new UnavailableIDException("영문 대소문자 및 숫자 외에 다른 문자는 포함될 수 없습니다.");
+		
+		
+		String encodedPassword = passwordEncoder.encode(newUser.getPassword());
+		
+		UserDetails user = new User(id, encodedPassword,
 				Arrays.asList(new SimpleGrantedAuthority("USER")));
 		
 		try {
